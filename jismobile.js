dc117822cabffe5d1417896ac6d99129dc081424
dashio <dahio2015@gmail.com>
2017-01-22 18:53:30 +0900

    [update] this error

--- jismobile.js
+++ jismobile.js
@@ -3,7 +3,7 @@
 		var ua = navigator.userAgent;
 		return this.each(function(){
 
-			var self = this;
+			var self = $(this);
 
 			if (ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0) {
 				self.addClass("sp");
