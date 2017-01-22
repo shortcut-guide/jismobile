47207ce9ccf0886b16fea93dbcc9b164944538c9
dashio <dahio2015@gmail.com>
2017-01-22 18:49:26 +0900

    [update]addclass

--- jismobile.js
+++ jismobile.js
@@ -2,12 +2,15 @@
 	jQuery.fn.jismobile = function(){
 		var ua = navigator.userAgent;
 		return this.each(function(){
+
+			var self = this;
+
 			if (ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0) {
-				return 'sp';
+				self.addClass("sp");
 			} else if (ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0) {
-				return 'tablet';
+				self.addClass('tablet');
 			} else {
-				return 'pc';
+				self.addClass('pc');
 			}
 		});
 	}
