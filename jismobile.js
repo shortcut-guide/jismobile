2caae78ebd8252a70848b6559be5784788052bbd
dashio <dahio2015@gmail.com>
2017-01-22 18:10:50 +0900

    gac

---
+++ jismobile.js
@@ -0,0 +1,12 @@
+(function( $ ) {
+	function jismobile(){
+		var ua = navigator.userAgent;
+		if (ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0) {
+			return 'sp';
+		} else if (ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0) {
+			return 'tablet';
+		} else {
+			return 'pc';
+		}
+	}
+})( jQuery );
