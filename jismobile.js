e9ff5761f22c7c51d9dc199c3aaae486c3d15316
dashio <dahio2015@gmail.com>
2017-01-22 18:41:38 +0900

    [update] プラグイン化

--- jismobile.js
+++ jismobile.js
@@ -1,12 +1,14 @@
 (function( $ ) {
-	function jismobile(){
+	jQuery.fn.jismobile = function(){
 		var ua = navigator.userAgent;
-		if (ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0) {
-			return 'sp';
-		} else if (ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0) {
-			return 'tablet';
-		} else {
-			return 'pc';
-		}
+		return this.each(function(){
+			if (ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0) {
+				return 'sp';
+			} else if (ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0) {
+				return 'tablet';
+			} else {
+				return 'pc';
+			}
+		});
 	}
 })( jQuery );
